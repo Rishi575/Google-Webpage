162d438738c273d5053dc615d75b8c9962d90655
Rishi575 <rishichidory@gmail.com>
2024-06-26 13:27:29 +0530

    ubdated

--- google webpage/script.js
+++
@@ -1,7 +0,0 @@
-function performSearch() {
-    const query = document.getElementById('APjFqb').value;
-    if (query.trim()!== '') {
-        const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-        window.open(url, '_blank');
-    }
-}
